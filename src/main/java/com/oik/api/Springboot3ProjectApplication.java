467e55cdec3e13d8e3583c0e4c7dd09cf686a699
oikaze <2889295938@qq.com>
2023-05-02 17:33:28 +0800

    init

---
+++ src/main/java/com/oik/api/Springboot3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.oik.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oik/api/Springboot3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oik.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
